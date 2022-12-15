0c5ee0520431df568055fcb3cc42a7b8b7a8f45d
Berselis Mendoza <berselismendoza@gmail.com>
2022-12-15 08:26:18 -0400

    deploy
    update code

---
+++ Controllers/CargaHorariaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimeKeeper.Controllers
+{
+    [Authorize]
+    public class CargaHorariaController : Controller
+    {
+        
+        public IActionResult PanelCarga() => View();
+    }
+}
